e10ab12a52c5a8212819968fc456167d236828a0
FranciscoRubioD <franciscorubioprojects@gmail.com>
2024-12-27 14:22:56 -0500

    db connection 2

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ const dbConexion = mysql.createConnection({
   host:'190.140.248.133',
   user:'fran',
   password:'franWinston@2518!!',
-  database:'aguilafc'
+  database:'aguilafc',
+  port: 3306,  // Asegúrate de que el puerto sea el correcto
 })
 
 // manejo de error BD
