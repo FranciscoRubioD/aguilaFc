8d5277e7c9a20ebf8327552ea0c3665ecdfaa641
FranciscoRubioD <franciscorubioprojects@gmail.com>
2025-01-04 18:09:56 -0500

    aguila railway2

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql');
 
 // conexion a la base de datos
 const dbConexion = mysql.createConnection({
-  host:'190.140.248.133',
-  user:'fran',
-  password:'franWinston@2518!!',
-  database:'aguilafc',
-  port: 3306,  // Asegúrate de que el puerto sea el correcto
+  host:'viaduct.proxy.rlwy.net',
+  user:'root',
+  password:'cjiVOjnNolZkWwWUQKnBdcWFouXjkzpt',
+  database:'mySqlrailway',
+  port: 26491,  // Asegúrate de que el puerto sea el correcto
 })
 
 // manejo de error BD
