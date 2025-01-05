892c49278d8b3c3c9dfb38272738ffb0bbf2b7e9
FranciscoRubioD <franciscorubioprojects@gmail.com>
2025-01-04 21:52:50 -0500

    barra ganadora4

--- static/js/asistencia.js
+++ static/js/asistencia.js
@@ -1717,10 +1717,12 @@ function formatFechaHora(fechaUTC, hora) {
 function actualizarEstadisticas(g, e, p) {
   const total = g + e + p;  // Total de partidos
 
+  
+
   const minWidth = 5; // Ancho mínimo en porcentaje
-  const porcentajeGanadas = Math.max((g / total) * 100, minWidth);
-  const porcentajeEmpates = Math.max((e / total) * 100, minWidth);
-  const porcentajePerdidas = Math.max((p / total) * 100, minWidth);
+  const porcentajeGanadas = Math.max((g / total) * 10, minWidth);
+  const porcentajeEmpates = Math.max((e / total) * 10, minWidth);
+  const porcentajePerdidas = Math.max((p / total) * 10, minWidth);
 
   // Asigna los anchos de cada segmento basado en los porcentajes
   document.getElementById('ganadas').style.width = porcentajeGanadas + '%';
